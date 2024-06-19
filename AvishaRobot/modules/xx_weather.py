fd7d4707c34b488928adcc55489e162548a8846a
RONINEMX20 <168336946+RONINEMX20@users.noreply.github.com>
2024-06-19 12:53:06 +0530

    Update xx_weather.py

--- AvishaRobot/modules/xx_weather.py
+++ AvishaRobot/modules/xx_weather.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/lichaXbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/avishaXbot?startgroup=true"),
     ],
 ]
 
