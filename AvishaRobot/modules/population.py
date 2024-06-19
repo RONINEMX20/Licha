fa8382da5813ccd77e65999568104424275374db
RONINEMX20 <168336946+RONINEMX20@users.noreply.github.com>
2024-06-19 11:54:46 +0530

    Update population.py

--- AvishaRobot/modules/population.py
+++ AvishaRobot/modules/population.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/botXaid?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/lichaxbot?startgroup=true"),
     ],
 ]
 
