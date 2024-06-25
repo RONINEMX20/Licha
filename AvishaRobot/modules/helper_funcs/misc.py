9a88bb5bb8fa0e89f8692136a8b094b29d74962d
RONINEMX20 <168336946+RONINEMX20@users.noreply.github.com>
2024-06-25 23:35:08 +0530

    Update misc.py

--- AvishaRobot/modules/helper_funcs/misc.py
+++ AvishaRobot/modules/helper_funcs/misc.py
@@ -1,207 +1,62 @@
-from math import ceil
-from typing import Dict, List
-from uuid import uuid4
+import logging
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler, CallbackContext
 
-from telegram import (
-    MAX_MESSAGE_LENGTH,
-    Bot,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    InlineQueryResultArticle,
-    InputTextMessageContent,
-    ParseMode,
-)
-from telegram.error import TelegramError
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-from AvishaRobot import NO_LOAD
+# Telegram bot token
+TOKEN = 'YOUR_BOT_TOKEN'
 
+# Command handler for /start
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Hello! I am your bot. Send me a message or use /help to see available commands.')
 
-class EqInlineKeyboardButton(InlineKeyboardButton):
-    def __eq__(self, other):
-        return self.text == other.text
+# Command handler for /help
+def help_command(update: Update, context: CallbackContext):
+    update.message.reply_text('Available commands:\n/start - Start the bot\n/help - Show this help message')
 
-    def __lt__(self, other):
-        return self.text < other.text
+# Handler for unknown commands
+def unknown_command(update: Update, context: CallbackContext):
+    update.message.reply_text("Sorry, I didn't understand that command. Use /help to see available commands.")
 
-    def __gt__(self, other):
-        return self.text > other.text
+# Handler for regular text messages
+def text_message(update: Update, context: CallbackContext):
+    update.message.reply_text(f'You said: {update.message.text}')
 
-
-def split_message(msg: str) -> List[str]:
-    if len(msg) < MAX_MESSAGE_LENGTH:
-        return [msg]
-
-    lines = msg.splitlines(True)
-    small_msg = ""
-    result = []
-    for line in lines:
-        if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
-            small_msg += line
-        else:
-            result.append(small_msg)
-            small_msg = line
-    else:
-        # Else statement at the end of the for loop, so append the leftover string.
-        result.append(small_msg)
-
-    return result
-
-
-def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
-    if not chat:
-        modules = sorted(
-            [
-                EqInlineKeyboardButton(
-                    x.__mod_name__,
-                    callback_data="{}_module({})".format(
-                        prefix, x.__mod_name__.lower()
-                    ),
-                )
-                for x in module_dict.values()
-            ]
-        )
-    else:
-        modules = sorted(
-            [
-                EqInlineKeyboardButton(
-                    x.__mod_name__,
-                    callback_data="{}_module({},{})".format(
-                        prefix, chat, x.__mod_name__.lower()
-                    ),
-                )
-                for x in module_dict.values()
-            ]
-        )
-
-    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
-
-    round_num = len(modules) / 3
-    calc = len(modules) - round(round_num)
-    if calc in [1, 2]:
-        pairs.append((modules[-1],))
-
-    max_num_pages = ceil(len(pairs) / 4)
-    modulo_page = page_n % max_num_pages
-
-    # can only have a certain amount of buttons side by side
-    if len(pairs) > 3:
-        pairs = pairs[modulo_page * 5: 5* (modulo_page + 1)] + [
-            (
-                EqInlineKeyboardButton(
-                    "◁◁", callback_data="{}_prev({})".format(prefix, modulo_page)
-                ),
-                EqInlineKeyboardButton(
-                    "ʜᴏᴍᴇ", callback_data="mukesh_back"
-                ),
-                EqInlineKeyboardButton(
-                    "ɴᴇxᴛ", callback_data="{}_next({})".format(prefix, modulo_page)
-                ),
-            )
-        ]
-
-    else:
-        pairs += [[EqInlineKeyboardButton("ʙᴀᴄᴋ", callback_data="mukesh_back")]]
-
-    return pairs
-
-
-def article(
-    title: str = "",
-    description: str = "",
-    message_text: str = "",
-    thumb_url: str = None,
-    reply_markup: InlineKeyboardMarkup = None,
-    disable_web_page_preview: bool = False,
-) -> InlineQueryResultArticle:
-
-    return InlineQueryResultArticle(
-        id=uuid4(),
-        title=title,
-        description=description,
-        thumb_url=thumb_url,
-        input_message_content=InputTextMessageContent(
-            message_text=message_text,
-            disable_web_page_preview=disable_web_page_preview,
+# Handler for inline queries
+def inline_query(update: Update, context: CallbackContext):
+    query = update.inline_query.query
+    results = [
+        InlineQueryResultArticle(
+            id='1', title='Caps', input_message_content=InputTextMessageContent(query.upper())
         ),
-        reply_markup=reply_markup,
-    )
+        InlineQueryResultArticle(
+            id='2', title='Bold', input_message_content=InputTextMessageContent(f'*{query}*'), parse_mode='Markdown'
+        )
+    ]
+    update.inline_query.answer(results)
 
+# Initialize the Updater and Dispatcher
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-def send_to_list(
-    bot: Bot, send_to: list, message: str, markdown=False, html=False
-) -> None:
-    if html and markdown:
-        raise Exception("Can only send with either markdown or HTML!")
-    for user_id in set(send_to):
-        try:
-            if markdown:
-                bot.send_message(user_id, message, parse_mode=ParseMode.MARKDOWN)
-            elif html:
-                bot.send_message(user_id, message, parse_mode=ParseMode.HTML)
-            else:
-                bot.send_message(user_id, message)
-        except TelegramError:
-            pass  # ignore users who fail
+# Register command handlers
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('help', help_command))
 
+# Register handler for unknown commands/messages
+dispatcher.add_handler(MessageHandler(Filters.command, unknown_command))
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, text_message))
 
-def build_keyboard(buttons):
-    keyb = []
-    for btn in buttons:
-        if btn.same_line and keyb:
-            keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
-        else:
-            keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
+# Register inline query handler
+dispatcher.add_handler(InlineQueryHandler(inline_query))
 
-    return keyb
+# Main function to start the bot
+def main():
+    updater.start_polling()
+    logging.info("Bot started polling.")
+    updater.idle()
 
-
-def revert_buttons(buttons):
-    res = ""
-    for btn in buttons:
-        if btn.same_line:
-            res += "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
-        else:
-            res += "\n[{}](buttonurl://{})".format(btn.name, btn.url)
-
-    return res
-
-
-def build_keyboard_parser(bot, chat_id, buttons):
-    keyb = []
-    for btn in buttons:
-        if btn.url == "{rules}":
-            btn.url = "http://t.me/{}?start={}".format(bot.username, chat_id)
-        if btn.same_line and keyb:
-            keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
-        else:
-            keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
-
-    return keyb
-
-
-def user_bot_owner(func):
-    @wraps(func)
-    def is_user_bot_owner(bot: Bot, update: Update, *args, **kwargs):
-        user = update.effective_user
-        if user and user.id == OWNER_ID:
-            return func(bot, update, *args, **kwargs)
-        else:
-            pass
-
-    return is_user_bot_owner
-
-
-def build_keyboard_alternate(buttons):
-    keyb = []
-    for btn in buttons:
-        if btn[2] and keyb:
-            keyb[-1].append(InlineKeyboardButton(btn[0], url=btn[1]))
-        else:
-            keyb.append([InlineKeyboardButton(btn[0], url=btn[1])])
-
-    return keyb
-
-
-def is_module_loaded(name):
-    return name not in NO_LOAD
-                                 
+if __name__ == '__main__':
+    main()
