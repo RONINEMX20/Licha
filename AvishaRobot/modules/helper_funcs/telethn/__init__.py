931c8715fb1c7dfeb93538d4ebddf1250d6fd4a0
RONINEMX20 <168336946+RONINEMX20@users.noreply.github.com>
2024-06-25 23:18:26 +0530

    Update __init__.py

--- AvishaRobot/modules/helper_funcs/telethn/__init__.py
+++ AvishaRobot/modules/helper_funcs/telethn/__init__.py
@@ -1,7 +1,36 @@
-from AvishaRobot import DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES, telethn
+import os
+import logging
+from pymongo import MongoClient
+from telegram.ext import Updater
 
-IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-IMMUNE_USERS = (
-    list(DRAGONS) + list(WOLVES) + list(DEMONS) + list(TIGERS) + list(DEV_USERS)
-)
+# Telegram bot token
+TOKEN = 'YOUR_BOT_TOKEN'
+
+# MongoDB connection
+mongo_client = MongoClient('mongodb://localhost:27017/')
+db = mongo_client['telegram_bot_db']
+
+# Initialize the Updater and Dispatcher
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+# Import command handlers from separate modules
+from . import misc, permissions, sections, wel_db
+
+# Register command handlers from imported modules
+dispatcher.add_handler(misc.dispatcher)
+dispatcher.add_handler(permissions.dispatcher)
+dispatcher.add_handler(sections.dispatcher)
+dispatcher.add_handler(wel_db.dispatcher)
+
+# Main function to start the bot
+def main():
+    updater.start_polling()
+    logging.info("Bot started polling.")
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
