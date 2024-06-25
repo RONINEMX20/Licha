6d4813f01fd8f04320291799c4c4e2b8dc200973
RONINEMX20 <168336946+RONINEMX20@users.noreply.github.com>
2024-06-25 23:32:52 +0530

    Update handlers.py

--- AvishaRobot/modules/helper_funcs/handlers.py
+++ AvishaRobot/modules/helper_funcs/handlers.py
@@ -1,139 +1,51 @@
-from pyrate_limiter import (
-    BucketFullException,
-    Duration,
-    Limiter,
-    MemoryListBucket,
-    RequestRate,
-)
+import logging
 from telegram import Update
-from telegram.ext import CommandHandler, Filters, MessageHandler, RegexHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-import AvishaRobot.modules.sql.blacklistusers_sql as sql
-from AvishaRobot import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-if ALLOW_EXCL:
-    CMD_STARTERS = ("/", "!")
-else:
-    CMD_STARTERS = "/"
+# Telegram bot token
+TOKEN = 'YOUR_BOT_TOKEN'
 
+# Command handler for /start
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Hello! I am your bot. Send me a message or use /help to see available commands.')
 
-class AntiSpam:
-    def __init__(self):
-        self.whitelist = (
-            (DEV_USERS or [])
-            + (DRAGONS or [])
-            + (WOLVES or [])
-            + (DEMONS or [])
-            + (TIGERS or [])
-        )
-        # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
-        Duration.CUSTOM = 15  # Custom duration, 15 seconds
-        self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
-        self.min_limit = RequestRate(20, Duration.MINUTE)  # 20 / Per minute
-        self.hour_limit = RequestRate(100, Duration.HOUR)  # 100 / Per hour
-        self.daily_limit = RequestRate(1000, Duration.DAY)  # 1000 / Per day
-        self.limiter = Limiter(
-            self.sec_limit,
-            self.min_limit,
-            self.hour_limit,
-            self.daily_limit,
-            bucket_class=MemoryListBucket,
-        )
+# Command handler for /help
+def help_command(update: Update, context: CallbackContext):
+    update.message.reply_text('Available commands:\n/start - Start the bot\n/help - Show this help message')
 
-    def check_user(self, user):
-        """
-        Return True if user is to be ignored else False
-        """
-        if user in self.whitelist:
-            return False
-        try:
-            self.limiter.try_acquire(user)
-            return False
-        except BucketFullException:
-            return True
+# Message handler for text messages
+def text_message(update: Update, context: CallbackContext):
+    update.message.reply_text(f'You said: {update.message.text}')
 
+# Message handler for photo messages
+def photo_message(update: Update, context: CallbackContext):
+    update.message.reply_text('Nice photo!')
 
-SpamChecker = AntiSpam()
-MessageHandlerChecker = AntiSpam()
+# Message handler for document messages
+def document_message(update: Update, context: CallbackContext):
+    update.message.reply_text('Thanks for the document!')
 
+# Initialize the Updater and Dispatcher
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-class CustomCommandHandler(CommandHandler):
-    def __init__(self, command, callback, admin_ok=False, allow_edit=False, **kwargs):
-        super().__init__(command, callback, **kwargs)
+# Register command handlers
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('help', help_command))
 
-        if allow_edit is False:
-            self.filters &= ~(
-                Filters.update.edited_message | Filters.update.edited_channel_post
-            )
+# Register message handlers
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, text_message))
+dispatcher.add_handler(MessageHandler(Filters.photo, photo_message))
+dispatcher.add_handler(MessageHandler(Filters.document, document_message))
 
-    def check_update(self, update):
-        if isinstance(update, Update) and update.effective_message:
-            message = update.effective_message
+# Main function to start the bot
+def main():
+    updater.start_polling()
+    logging.info("Bot started polling.")
+    updater.idle()
 
-            try:
-                user_id = update.effective_user.id
-            except:
-                user_id = None
-
-            if user_id:
-                if sql.is_user_blacklisted(user_id):
-                    return False
-
-            if message.text and len(message.text) > 1:
-                fst_word = message.text.split(None, 1)[0]
-                if len(fst_word) > 1 and any(
-                    fst_word.startswith(start) for start in CMD_STARTERS
-                ):
-
-                    args = message.text.split()[1:]
-                    command = fst_word[1:].split("@")
-                    command.append(message.bot.username)
-                    if user_id == 1087968824:
-                        user_id = update.effective_chat.id
-                    if not (
-                        command[0].lower() in self.command
-                        and command[1].lower() == message.bot.username.lower()
-                    ):
-                        return None
-                    if SpamChecker.check_user(user_id):
-                        return None
-                    filter_result = self.filters(update)
-                    if filter_result:
-                        return args, filter_result
-                    else:
-                        return False
-
-    def handle_update(self, update, dispatcher, check_result, context=None):
-        if context:
-            self.collect_additional_context(context, update, dispatcher, check_result)
-            return self.callback(update, context)
-        else:
-            optional_args = self.collect_optional_args(dispatcher, update, check_result)
-            return self.callback(dispatcher.bot, update, **optional_args)
-
-    def collect_additional_context(self, context, update, dispatcher, check_result):
-        if isinstance(check_result, bool):
-            context.args = update.effective_message.text.split()[1:]
-        else:
-            context.args = check_result[0]
-            if isinstance(check_result[1], dict):
-                context.update(check_result[1])
-
-
-class CustomRegexHandler(RegexHandler):
-    def __init__(self, pattern, callback, friendly="", **kwargs):
-        super().__init__(pattern, callback, **kwargs)
-
-
-class CustomMessageHandler(MessageHandler):
-    def __init__(self, filters, callback, friendly="", allow_edit=False, **kwargs):
-        super().__init__(filters, callback, **kwargs)
-        if allow_edit is False:
-            self.filters &= ~(
-                Filters.update.edited_message | Filters.update.edited_channel_post
-            )
-
-        def check_update(self, update):
-            if isinstance(update, Update) and update.effective_message:
-                return self.filters(update)
-                  
+if __name__ == '__main__':
+    main()
