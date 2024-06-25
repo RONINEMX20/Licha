de3fc713bcc049f97da30fb2a1ad9e9370469076
RONINEMX20 <168336946+RONINEMX20@users.noreply.github.com>
2024-06-25 23:28:51 +0530

    Update extraction.py

--- AvishaRobot/modules/helper_funcs/extraction.py
+++ AvishaRobot/modules/helper_funcs/extraction.py
@@ -1,171 +1,60 @@
-from typing import List, Optional
+import logging
+import re
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-from telegram import Message, MessageEntity
-from telegram.error import BadRequest
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-from AvishaRobot import LOGGER
-from AvishaRobot.modules.users import get_user_id
+# Telegram bot token
+TOKEN = 'YOUR_BOT_TOKEN'
 
+# Command handler for /start
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Hello! Send me a message with URLs and hashtags to extract.')
 
-def id_from_reply(message):
-    prev_message = message.reply_to_message
-    if not prev_message:
-        return None, None
-    user_id = prev_message.from_user.id
-    res = message.text.split(None, 1)
-    if len(res) < 2:
-        return user_id, ""
-    return user_id, res[1]
+# Message handler for extracting URLs and hashtags
+def extract_info(update: Update, context: CallbackContext):
+    message = update.message.text
+    
+    # Extract URLs using regex
+    urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message)
+    
+    # Extract hashtags using regex
+    hashtags = re.findall(r'#(\w+)', message)
 
+    # Prepare response message
+    response = ""
+    if urls:
+        response += f"URLs found:\n"
+        for url in urls:
+            response += f"{url}\n"
+    if hashtags:
+        response += f"Hashtags found:\n"
+        for tag in hashtags:
+            response += f"#{tag}\n"
+    
+    # If no URLs or hashtags found
+    if not urls and not hashtags:
+        response = "No URLs or hashtags found in the message."
 
-def extract_user(message: Message, args: List[str]) -> Optional[int]:
-    return extract_user_and_text(message, args)[0]
+    update.message.reply_text(response)
 
+# Initialize the Updater and Dispatcher
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-def extract_user_and_text(
-    message: Message, args: List[str]
-) -> (Optional[int], Optional[str]):
-    prev_message = message.reply_to_message
-    split_text = message.text.split(None, 1)
+# Register command handler
+dispatcher.add_handler(CommandHandler('start', start))
 
-    if len(split_text) < 2:
-        return id_from_reply(message)  # only option possible
+# Register message handler for all text messages
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, extract_info))
 
-    text_to_parse = split_text[1]
+# Main function to start the bot
+def main():
+    updater.start_polling()
+    logging.info("Bot started polling.")
+    updater.idle()
 
-    text = ""
-
-    entities = list(message.parse_entities([MessageEntity.TEXT_MENTION]))
-    ent = entities[0] if entities else None
-    # if entity offset matches (command end/text start) then all good
-    if entities and ent and ent.offset == len(message.text) - len(text_to_parse):
-        ent = entities[0]
-        user_id = ent.user.id
-        text = message.text[ent.offset + ent.length :]
-
-    elif len(args) >= 1 and args[0][0] == "@":
-        user = args[0]
-        user_id = get_user_id(user)
-        if not user_id:
-            message.reply_text(
-                "No idea who this user is. You'll be able to interact with them if "
-                "you reply to that person's message instead, or forward one of that user's messages."
-            )
-            return None, None
-
-        else:
-            user_id = user_id
-            res = message.text.split(None, 2)
-            if len(res) >= 3:
-                text = res[2]
-
-    elif len(args) >= 1 and args[0].isdigit():
-        user_id = int(args[0])
-        res = message.text.split(None, 2)
-        if len(res) >= 3:
-            text = res[2]
-
-    elif prev_message:
-        user_id, text = id_from_reply(message)
-
-    else:
-        return None, None
-
-    try:
-        message.bot.get_chat(user_id)
-    except BadRequest as excp:
-        if excp.message in ("User_id_invalid", "Chat not found"):
-            message.reply_text(
-                "I don't seem to have interacted with this user before - please forward a message from "
-                "them to give me control! (like a voodoo doll, I need a piece of them to be able "
-                "to execute certain commands...)"
-            )
-        else:
-            LOGGER.exception("Exception %s on user %s", excp.message, user_id)
-
-        return None, None
-
-    return user_id, text
-
-
-def extract_text(message) -> str:
-    return (
-        message.text
-        or message.caption
-        or (message.sticker.emoji if message.sticker else None)
-    )
-
-
-def extract_unt_fedban(
-    message: Message, args: List[str]
-) -> (Optional[int], Optional[str]):
-    prev_message = message.reply_to_message
-    split_text = message.text.split(None, 1)
-
-    if len(split_text) < 2:
-        return id_from_reply(message)  # only option possible
-
-    text_to_parse = split_text[1]
-
-    text = ""
-
-    entities = list(message.parse_entities([MessageEntity.TEXT_MENTION]))
-    ent = entities[0] if entities else None
-    # if entity offset matches (command end/text start) then all good
-    if entities and ent and ent.offset == len(message.text) - len(text_to_parse):
-        ent = entities[0]
-        user_id = ent.user.id
-        text = message.text[ent.offset + ent.length :]
-
-    elif len(args) >= 1 and args[0][0] == "@":
-        user = args[0]
-        user_id = get_user_id(user)
-        if not user_id and not isinstance(user_id, int):
-            message.reply_text(
-                "I don't have that user in my db.  "
-                "You'll be able to interact with them if you reply to that person's message instead, or forward one of that user's messages."
-            )
-            return None, None
-
-        else:
-            user_id = user_id
-            res = message.text.split(None, 2)
-            if len(res) >= 3:
-                text = res[2]
-
-    elif len(args) >= 1 and args[0].isdigit():
-        user_id = int(args[0])
-        res = message.text.split(None, 2)
-        if len(res) >= 3:
-            text = res[2]
-
-    elif prev_message:
-        user_id, text = id_from_reply(message)
-
-    else:
-        return None, None
-
-    try:
-        message.bot.get_chat(user_id)
-    except BadRequest as excp:
-        if excp.message in ("User_id_invalid", "Chat not found") and not isinstance(
-            user_id, int
-        ):
-            message.reply_text(
-                "I don't seem to have interacted with this user before "
-                "please forward a message from them to give me control! "
-                "(like a voodoo doll, I need a piece of them to be able to execute certain commands...)"
-            )
-            return None, None
-        elif excp.message != "Chat not found":
-            LOGGER.exception("Exception %s on user %s", excp.message, user_id)
-            return None, None
-        elif not isinstance(user_id, int):
-            return None, None
-
-    return user_id, text
-
-
-def extract_user_fban(message: Message, args: List[str]) -> Optional[int]:
-    return extract_unt_fedban(message, args)[0]
-              
+if __name__ == '__main__':
+    main()
